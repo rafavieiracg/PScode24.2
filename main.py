cd4e6d928db478dcb57c39fb6a20136710043f36
Raphael Vieira <86532443+rafavieiracg@users.noreply.github.com>
2024-08-07 17:56:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+    EN:
+    Main module (The one you really need to run)
+    PT:
+    Módulo principal (Aquele que você realmente precisa rodar)
+'''
+
+from src.defs import send_email
+
+if __name__ == "__main__":
+    #          Sender, Key-pass,  Discord Link
+    send_email('', '', '')
